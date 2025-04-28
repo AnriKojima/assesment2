8ee5c55822e61e43b91cede6724d3d8552bbb495
Anri <anrikojima09@gmail.com>
2025-04-28 20:30:15 +0700

    Menyiapkan data di ViewModel

---
+++ app/src/main/java/com/anri0145/assesment2/model/Pengeluaran.kt
@@ -0,0 +1,8 @@
+package com.anri0145.assesment2.model
+
+data class Pengeluaran(
+    val id: Long,
+    val judul: String,
+    val belanja: String,
+    val tanggal: String
+)
---
+++ app/src/main/java/com/anri0145/assesment2/screen/MainViewModel.kt
@@ -0,0 +1,16 @@
+package com.anri0145.assesment2.screen
+
+import androidx.lifecycle.ViewModel
+import com.anri0145.assesment2.model.Pengeluaran
+
+class MainViewModel : ViewModel() {
+
+    val data = listOf(
+        Pengeluaran(
+            1,
+            "sdfghjkl",
+            "xcfghjkiuytf",
+            "dfghjkl"
+        )
+    )
+}
