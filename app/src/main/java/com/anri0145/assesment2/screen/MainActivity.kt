d15346859251bfde6dece001af7276891d593231
Anri <anrikojima09@gmail.com>
2025-04-29 20:50:49 +0700

    Menyimpan preference pengguna

--- app/src/main/java/com/anri0145/assesment2/screen/MainActivity.kt
+++ app/src/main/java/com/anri0145/assesment2/screen/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.anri0145.assesment2.navigation.SetupNavGraph
 import com.anri0145.assesment2.ui.theme.Assesment2Theme
 
 class MainActivity : ComponentActivity() {
@@ -12,7 +13,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Assesment2Theme {
-                MainScreen()
+                SetupNavGraph()
             }
         }
     }
