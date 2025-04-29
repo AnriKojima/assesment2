1953a19f83dca5d59f994365a80e67dcd5f68de1
Anri <anrikojima09@gmail.com>
2025-04-29 12:05:20 +0700

    Menangani keadaan jika list kosong

--- app/src/main/java/com/anri0145/assesment2/screen/MainScreen.kt
+++ app/src/main/java/com/anri0145/assesment2/screen/MainScreen.kt
@@ -1,6 +1,8 @@
 package com.anri0145.assesment2.screen
 
 import android.content.res.Configuration
+import android.widget.Toast
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -18,6 +20,7 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -53,6 +56,7 @@ fun MainScreen() {
 fun ScreenContent(modifier: Modifier = Modifier){
     val viewModel: MainViewModel = viewModel()
     val data = emptyList<Pengeluaran>()
+    val context = LocalContext.current
 
     if (data.isEmpty()){
         Column(
@@ -68,7 +72,10 @@ fun ScreenContent(modifier: Modifier = Modifier){
             modifier = modifier.fillMaxSize()
         ){
             items(data){
-                ListItem(pengeluaran = it)
+                ListItem(pengeluaran = it){
+                    val pesan = context.getString(R.string.x_diklik, it.judul)
+                    Toast.makeText(context, pesan, Toast.LENGTH_SHORT).show()
+                }
                 HorizontalDivider()
             }
         }
@@ -77,9 +84,11 @@ fun ScreenContent(modifier: Modifier = Modifier){
 
 
 @Composable
-fun ListItem(pengeluaran: Pengeluaran){
+fun ListItem(pengeluaran: Pengeluaran, onClick: () -> Unit){
     Column (
-        modifier = Modifier.fillMaxWidth().padding(16.dp),
+        modifier = Modifier.fillMaxWidth()
+            .clickable { onClick() }
+            .padding(16.dp),
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ){
         Text(
